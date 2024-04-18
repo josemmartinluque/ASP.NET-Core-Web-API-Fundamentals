e7f4e7ec7c570c11491c9d95316cdf1385bc6a55
josemmartinluque <jose.m.martin.luque@avanade.com>
2024-04-18 08:56:57 +0200

    Module 8 completed

--- CityInfo.API/Services/ICityInfoRepository.cs
+++ CityInfo.API/Services/ICityInfoRepository.cs
@@ -5,10 +5,12 @@ namespace CityInfo.API.Services
     public interface ICityInfoRepository
     {
         Task<IEnumerable<City>> GetCitiesAsync();
+        Task<(IEnumerable<City>, PaginationMetadata)> GetCitiesAsync(
+            string? name, string? searchQuery, int pageNumber, int pageSize);
         Task<City?> GetCityAsync(int cityId, bool includePointsOfInterest);
         Task<bool> CityExistsAsync(int cityId);
         Task<IEnumerable<PointOfInterest>> GetPointsOfInterestForCityAsync(int cityId);
-        Task<PointOfInterest?> GetPointOfInterestForCityAsync(int cityId, 
+        Task<PointOfInterest?> GetPointOfInterestForCityAsync(int cityId,
             int pointOfInterestId);
         Task AddPointOfInterestForCityAsync(int cityId, PointOfInterest pointOfInterest);
         void DeletePointOfInterest(PointOfInterest pointOfInterest);
